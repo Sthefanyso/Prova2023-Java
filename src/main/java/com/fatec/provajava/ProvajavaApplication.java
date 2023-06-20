df8923c90a0bc8bbed8f38b684514bb42e74e5a2
Sthefanyso <sthefany.silva3@fatec.sp.gov.br>
2023-06-20 10:29:20 -0300

    1 commit. projeto criado

---
+++ src/main/java/com/fatec/provajava/ProvajavaApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.provajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvajavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvajavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/provajava/ProvajavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.provajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvajavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
